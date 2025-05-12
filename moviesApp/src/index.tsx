6d1ca1331947bc9201fa389170102f34612ebd90
KatyaRomashko <skairlet93@gmail.com>
2025-05-12 16:55:10 +0100

    Movie Review and Upcoming

--- moviesApp/src/index.tsx
+++ moviesApp/src/index.tsx
@@ -4,23 +4,22 @@ import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
+import UpComingPage from "./pages/upComingPage";
+
 
 const App = () => {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favourites">Favourites</Link>
-        </li>
-      </ul>
+  <SiteHeader />    
       <Routes>
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+        <Route path="/movies/upcoming" element={<UpComingPage/>} />
       </Routes>
     </BrowserRouter>
   );
--- moviesApp/src/types/interfaces.ts
+++ moviesApp/src/types/interfaces.ts
@@ -50,4 +50,8 @@ export interface BaseMovieProps {
   export interface MovieListPageTemplateProps extends BaseMovieListProps {
     title: string;
   }
-  
+  export interface Review{
+    id: string;
+    content: string
+    author: string
+  }
