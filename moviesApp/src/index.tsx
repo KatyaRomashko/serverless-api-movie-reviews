d97a1e778a0fb81af6e4d3ee4a9db4dec51ee76e
KatyaRomashko <skairlet93@gmail.com>
2025-05-12 23:45:36 +0100

    Popular movie page

--- moviesApp/src/index.tsx
+++ moviesApp/src/index.tsx
@@ -11,6 +11,7 @@ import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage';
+import PopularMoviePage from "./pages/popularPage";
 
 
 const queryClient = new QueryClient({
@@ -37,6 +38,7 @@ const App = () => {
         <Route path="*" element={<Navigate to="/" />} />
         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
         <Route path="/movies/upcoming" element={<UpComingPage/>} />
+        <Route path="/movies/popular" element={<PopularMoviePage/>} />
         <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
       </Routes>
       </MoviesContextProvider>
